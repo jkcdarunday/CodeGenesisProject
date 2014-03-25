036ae705736961e4bdcc70d327bfb381f65f804a
Keith Certeza Darunday <jkcdarunday@gmail.com>
2014-03-25 19:43:36 +0900

    Creation and implementation of KSmoothVariable

---
+++ scripts/KObject/KSmoothVariable.js
@@ -0,0 +1,86 @@
+/*
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *   Boston, MA  02110-1301, USA.
+ *
+ *   ---
+ *   Copyright (C) 2014, Jan Keith Darunday <jkcdarunday@uplb.edu.ph>
+ */
+
+define(['KObject'], function(KObject){
+    return KObject.extend({
+        init: function(settings, parent){
+            this.constructor.super.call(this);
+            this.value = 0;
+            this.targetValue = 0;
+            this.gradientType = "IMMEDIATE";
+            this.gradientInterval = 0;
+            this.gradientTolerance = 0.01;
+            this.set(settings);
+            this.isEqual = true;
+            if(parent instanceof KObject)
+                KObject.connect(parent, 'updateVariables', this, 'update');
+        },
+        set: function(config){
+            if(config["value"]){
+                this.targetValue = config.value;
+                this.isEqual = false;
+            }
+            if(config["actualValue"]){
+                this.value = config.actualValue;
+                this.isEqual = false;
+            }
+            if(config["gradient"]){
+                if(config.gradient["type"]){
+                    this.gradientType = config.gradient.type;
+                    if(config.gradient.type == "EASE")
+                        this.gradientInterval = 18;
+                    else if(config.gradient.type == "LINEAR")
+                        this.gradientInterval = 0.1;
+                }
+                if(config.gradient["interval"])
+                    this.gradientInterval = config.gradient.interval;
+            }
+        },
+        getValue: function(){
+            return this.value;
+        },
+        slots:{
+            update: function(){
+                var previousEqualityState, currentEqualityState;
+                if(previousEqualityState = this.value != this.targetValue){
+                    var gradientDelta = this.targetValue-this.value;
+                    if(this.gradientType == 'IMMEDIATE'){
+                        this.value = this.targetValue;
+                    }else if(this.gradientType == 'LINEAR'){
+                        if(this.targetValue < this.value)
+                            this.value -= this.gradientInterval;
+                        else
+                            this.value += this.gradientInterval;
+                    } else if (this.gradientType == 'EASE'){
+                        this.value += gradientDelta/this.gradientInterval;
+                    }
+                    if(Math.abs(gradientDelta) < this.gradientTolerance)
+                        this.value = this.targetValue;
+                }
+                if(currentEqualityState = this.value == this.targetValue){
+                    this.isEqual = true;
+                } else {
+                    this.isEqual = false;
+                }
+                if(previousEqualityState && currentEqualityState) this.emit("nowEqual");
+            }
+        }
+    });
+});
