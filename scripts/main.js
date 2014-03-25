083831ab0010874ff4cceee8de6555787f8c6b28
Keith Certeza Darunday <jkcdarunday@gmail.com>
2014-03-25 11:47:01 +0900

    Created test loading scene. It seems to work nicely.

--- scripts/main.js
+++ scripts/main.js
@@ -20,45 +20,72 @@ define(["KObject", "KThread", "KCanvas", "KScene", "KImage", "KBase"],function(K
 
     var cc = new KCanvas('#leCanvas', true);
     var cs = new KScene();
-    var heximg = new KImage('hexagon');
-    heximg.set({
-        type: 'NORMAL',
-        size:{x:100,y:115},
-        position:{
-            image:{x:0,y:0},
-        },
-        fade:{
-            type:'EASE',
-            interval:2
-        },
-        opacity:0.4,
-        animation:{
-            type:'EASE',
-            interval:8
+    var x = {};
+    var Positioner = new KThread(function(){
+        for(var zz in this.images){
+            if(zz == this.count%3-1)
+                this.images[this.count%3-1].set({
+                    opacity:1.0
+                });
+            else
+                this.images[zz].set({
+                    opacity:.5
+                });
         }
-    });
-    KObject.connect(cs, 'update', heximg, 'update');
-    KObject.connect(cs, 'draw', heximg, 'draw');
+        this.count ++ ;
+    }, 1000);
+    Positioner.count = 0;
+    Positioner.images = {};
+    for(var i = -1; i<=1;i++){
+        x[i] = new KImage('hexagon');
+        x[i].slots.resizedWindow = function(){
+            this.setTargetPosition(cc.canvasElement.width/2 + this.offset, cc.canvasElement.height/2);
+        };
+        x[i].offset = 120*i;
+        x[i].set({
+            type: 'NORMAL',
+            size:{x:100,y:115},
+            position:{
+                attachment:"CENTER",
+                image:{x:0,y:0},
+            },
+            fade:{
+                type:'EASE',
+                interval:32
+            },
+            opacity:0.5,
+            animation:{
+                type:'EASE',
+                 interval:8
+            }
+        });
+        KObject.connect(cc, "windowResized", x[i], "resizedWindow");
+        cs.addImage(x[i]);
+        Positioner.images[i] = x[i];
+    }
+    Positioner.start();
+
+
+
     cs.setBackground('fadeBG');
     cc.scene = cs;
     cc.start();
     cc.windowSet(window);
-    var Positioner = KObject.extend({
-        init:function(){
-            this.count=0;
-        },
-        slots:{
-            move:function(){
-                if(this.count % 4 == 0) this.kimage.setTargetPosition(400,400);
-                if(this.count % 4 == 1) this.kimage.setTargetPosition(400,100);
-                if(this.count % 4 == 2) this.kimage.setTargetPosition(100,400);
-                if(this.count % 4 == 3) this.kimage.setTargetPosition(100,100);
-                this.count++;
-            }
-        }
-    });
-    var p = new Positioner();
-    p.kimage = heximg;
-    KObject.connect(heximg, "inPosition", p, "move");
-    heximg.setTargetPosition(10,10);
+//     var Positioner = KObject.extend({
+//         init:function(){
+//             this.count=0;
+//         },
+//         slots:{
+//             move:function(){
+//                 if(this.count % 4 == 0) this.kimage.setTargetPosition(400,400);
+//                 if(this.count % 4 == 1) this.kimage.setTargetPosition(400,100);
+//                 if(this.count % 4 == 2) this.kimage.setTargetPosition(100,400);
+//                 if(this.count % 4 == 3) this.kimage.setTargetPosition(100,100);
+//                 this.count++;
+//             }
+//         }
+//     });
+//     var p = new Positioner();
+//     p.kimage = heximg;
+//     KObject.connect(heximg, "inPosition", p, "move");
 });
